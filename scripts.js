57855ff2dc3548d60bca1d946c0cedd1e09aa914
MrRutledge <rahimrutledge@gmail.com>
2018-08-30 13:23:01 +0100

    playing with colors

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 const cards = document.querySelectorAll('.memery-card');
 
+let hasFlippedCard = false;
+let firsCard, secondCard;
+
 function flipCard() {
   this.classList.toggle('flip');
+if (!hasFlippedCard) {
+  // first click
+  hasFlippedCard = true;
+  firsCard = this;
+}
+else{
+  // second Click
+  hasFlippedCard =flase;
+  secondCard =this;
+  
+  //
+}
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
